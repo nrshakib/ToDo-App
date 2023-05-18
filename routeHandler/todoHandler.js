f2ffb117626d27b3e568544942370e81c9cbee95
NRS\nrsha <nrshakib007@gmail.com>
2023-05-18 22:18:58 +0600

    single data insertion

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -8,8 +8,25 @@ const Todo = new mongoose.model("Todo", todoSchema);
 
 //todo routes
 router.get("/", async (req, res) => {});
-router.get("/:id", async (req, res) => {});
-router.post("/", async (req, res) => {});
+router.get("/:id", async (req, res) => { });
+
+//insert single data
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo
+    .save()
+    .then(() => {
+      res.status(200).json({
+        message: "Todo inserted succefully",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "An error occured",
+        err,
+      });
+    });
+});
 router.post("/all", async (req, res) => {});
 router.put("/:id", async (req, res) => {});
 router.delete("/:id", async (req, res) => {});
